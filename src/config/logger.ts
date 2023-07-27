459212aac4c64db670cbeac8d9a46c391ca3669a
arefinnuman <arefnnuman2000@gmail.com>
2023-07-27 11:10:54 +0600

    Agent Banking Added

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,6 +1,6 @@
-import path from "path";
-import { createLogger, format, transports } from "winston";
-import DailyRotateFile from "winston-daily-rotate-file";
+import path from 'path';
+import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 const { combine, timestamp, label, printf } = format;
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
@@ -11,53 +11,53 @@ const myFormat = printf(({ level, message, label, timestamp }) => {
 });
 
 const logger = createLogger({
-  level: "info",
+  level: 'info',
   format: combine(
     label({
-      label: "successLogs",
+      label: 'successLogs',
     }),
     timestamp(),
-    myFormat
+    myFormat,
   ),
   transports: [
     new transports.Console(),
     new DailyRotateFile({
       filename: path.join(
         process.cwd(),
-        "logs",
-        "successLogs",
-        "%DATE%_success.log"
+        'logs',
+        'successLogs',
+        '%DATE%_success.log',
       ),
-      datePattern: "YYYY-MM-DD-HH",
+      datePattern: 'YYYY-MM-DD-HH',
       zippedArchive: true,
-      maxSize: "20m",
-      maxFiles: "14d",
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 });
 
 const errorLogger = createLogger({
-  level: "error",
+  level: 'error',
   format: combine(
     label({
-      label: "errorLogs",
+      label: 'errorLogs',
     }),
     timestamp(),
-    myFormat
+    myFormat,
   ),
   transports: [
     new transports.Console(),
     new DailyRotateFile({
       filename: path.join(
         process.cwd(),
-        "logs",
-        "errorLogs",
-        "%DATE%_error.log"
+        'logs',
+        'errorLogs',
+        '%DATE%_error.log',
       ),
-      datePattern: "YYYY-MM-DD-HH",
+      datePattern: 'YYYY-MM-DD-HH',
       zippedArchive: true,
-      maxSize: "20m",
-      maxFiles: "14d",
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 });
